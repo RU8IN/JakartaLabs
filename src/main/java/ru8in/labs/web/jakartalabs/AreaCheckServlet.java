5856d3ebbb8c13681e631b156d7ee6bbb47bed4b
infinity <iskatel.03@mail.ru>
2024-02-18 18:51:07 +0300

    Добавлены сервлеты для лабораторной

---
+++ src/main/java/ru8in/labs/web/jakartalabs/AreaCheckServlet.java
@@ -0,0 +1,17 @@
+package ru8in.labs.web.jakartalabs;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class AreaCheckServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
+
---
+++ src/main/java/ru8in/labs/web/jakartalabs/ControllerServlet.java
@@ -0,0 +1,16 @@
+package ru8in.labs.web.jakartalabs;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class ControllerServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
